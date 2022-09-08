d239c6129ad69daf840da146510d7e0eab3e147b
unknown <nzr.frz@gmail.com>
2022-09-08 20:16:04 +0700

    nodejs gateway api playground first init

---
+++ app.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import useragent from "express-useragent";
+import helmet from "helmet"
+
+import routes from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+
+let corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+};
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(useragent.express());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes);
+
+app.get("/", (req, res) => {
+    res.send("!!! NODEJS GATEWAY API PLAYGROUND !!!");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App Running on: http://localhost:${process.env.PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import express from "express";
+import { readFile } from "fs/promises";
+
+const routes = express.Router();
+
+const REGISTRY = JSON.parse(
+    await readFile(new URL("../registry/registry.json", import.meta.url))
+);
+
+routes.all("/:serviceName/:path(*)?", (req, res) => {
+    const serviceData = REGISTRY.filter((data) => data.service === req.params.serviceName);
+    axios({
+        method: req.method,
+        baseURL: `${req.headers.host.includes("localhost") ? serviceData[0].LOCAL_BASE_PATH : serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
+        headers: {
+            authorization: req.headers.authorization || null,
+            browser: req.useragent.browser,
+            version: req.useragent.version,
+            os: req.useragent.os,
+            platform: req.useragent.platform,
+        },
+        data: req.body
+    })
+    .then((results) => {
+        res.status(results.data.status).send(results.data);
+    })
+    .catch((error) => {
+        res.status(error.response.data.status).send(error.response.data);
+    })
+});
+
+export default routes;
