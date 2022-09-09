276c31091ccc0befbf68868cab0da5612a4969a2
unknown <nzr.frz@gmail.com>
2022-09-09 09:45:57 +0700

    remove unused variable

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,11 @@ const REGISTRY = JSON.parse(
 );
 
 routes.all("/:serviceName/:path(*)?", (req, res) => {
-    console.log(req.params.serviceName + "/" + req.params.path);
     const serviceData = REGISTRY.filter((data) => data.service === req.params.serviceName);
     axios({
         method: req.method,
         baseURL: `${serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
         headers: {
-            // host: `${serviceData[0].HOST}`,
             authorization: req.headers.authorization || null,
             browser: req.useragent.browser,
             version: req.useragent.version,
