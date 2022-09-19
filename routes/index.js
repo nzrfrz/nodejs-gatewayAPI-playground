c933d2aac18151c846edc62478eac38346977c0c
nzrfrz <64332917+nzrfrz@users.noreply.github.com>
2022-09-19 19:38:15 +0700

    Update index.js
    adding try catch block

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,30 @@ const REGISTRY = JSON.parse(
     await readFile(new URL("../registry/registry.json", import.meta.url))
 );
 
-routes.all("/:serviceName/:path(*)?", (req, res) => {
+routes.all("/:serviceName/:path(*)?", async (req, res) => {
     const serviceData = REGISTRY.filter((data) => data.service === req.params.serviceName);
-    axios({
-        method: req.method,
-        baseURL: `${serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
-        headers: {
-            authorization: req.headers.authorization || null,
-            browser: req.useragent.browser,
-            version: req.useragent.version,
-            os: req.useragent.os,
-            platform: req.useragent.platform,
-        },
-        data: req.body
-    })
-    .then((results) => {
-        res.status(results.data.status).send(results.data);
-    })
-    .catch((error) => {
-        res.status(error.response.data.status).send(error.response.data);
-    })
+    try {
+        await axios({
+            method: req.method,
+            baseURL: `${serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
+            headers: {
+                authorization: req.headers.authorization || null,
+                browser: req.useragent.browser,
+                version: req.useragent.version,
+                os: req.useragent.os,
+                platform: req.useragent.platform,
+            },
+            data: req.body
+        })
+        .then((results) => {
+            res.status(results.data.status).send(results.data);
+        })
+        .catch((error) => {
+            res.status(error.response.data.status).send(error.response.data);
+        })
+    } catch(error) {
+        res.status(error.response.data.status).send(error);
+    }
 });
 
 export default routes;
