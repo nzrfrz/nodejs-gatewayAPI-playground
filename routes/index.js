888716161ec7bf2f84d8c64749cc62d07ae1adfa
nzrfrz <64332917+nzrfrz@users.noreply.github.com>
2022-09-19 19:46:52 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,24 @@ const REGISTRY = JSON.parse(
 
 routes.all("/:serviceName/:path(*)?", async (req, res) => {
     const serviceData = REGISTRY.filter((data) => data.service === req.params.serviceName);
-    try {
-        await axios({
-            method: req.method,
-            baseURL: `${serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
-            headers: {
-                authorization: req.headers.authorization || null,
-                browser: req.useragent.browser,
-                version: req.useragent.version,
-                os: req.useragent.os,
-                platform: req.useragent.platform,
-            },
-            data: req.body
-        })
-        .then((results) => {
-            res.status(results.data.status).send(results.data);
-        })
-        .catch((error) => {
-            res.status(error.response.data.status).send(error.response.data);
-        })
-    } catch(error) {
-        res.status(503).send(error);
-    }
+    await axios({
+        method: req.method,
+        baseURL: `${serviceData[0].BASE_PATH}/${req.params.serviceName}/${req.params.path}`,
+        headers: {
+            authorization: req.headers.authorization || null,
+            browser: req.useragent.browser,
+            version: req.useragent.version,
+            os: req.useragent.os,
+            platform: req.useragent.platform,
+        },
+        data: req.body
+    })
+    .then((results) => {
+        res.status(results.data.status).send(results.data);
+    })
+    .catch((error) => {
+        res.status(error.response.data.status || error.response.status).send(error.response.data);
+    })
 });
 
 export default routes;
